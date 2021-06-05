fb65f17698a07c07c5c125c1980dbc8248b43c59
roysrijan <roy.srijan@outlook.com>
2021-06-05 16:11:59 +0530

    http server

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const app = require("./src/app");
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const {
+    socketService
+} = require("./src/services")
 
-const server = app.listen(port, () => {
-  console.log(`Server connection on  http://127.0.0.1:${port}`);  // Server Connnected
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server connection on  http://127.0.0.1:${process.env.PORT}`);  // Server Connnected
 });
 
 // Send Notification API
-app.post('/send-notification', bodyParser, (req, res) => {
-    const notify = { status:'OK', data: req.body};
-    socket.emit('notification', notify); // Updates Live Notification
-    res.send(notify);
-});
+// app.post('/send-notification', bodyParser, (req, res) => {
+//     const notify = { status:'OK', data: req.body};
+//     socket.emit('notification', notify); // Updates Live Notification
+//     res.send(notify);
+// });
 
 // Socket Layer over Http Server
-const socket = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-});
-// On every Client Connection
-socket.on('connection', socket => {
-    console.log('Socket: client connected');
-});
+// const socket = socketService.useSocket(server, {
+//     cors: {
+//       origin: '*',
+//     }
+// });
+
